dd7692e82edd291d2a9c27be49bdc6429eed966d
Rahul <geosagsanbg@gmail.com>
2026-07-10 08:59:06 +0530

    patched dns issue

--- src/configs/transporter.js
+++ src/configs/transporter.js
@@ -2,6 +2,7 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  famliy: 4,
   auth: {
     user: process.env.PAPESEL_GMAIL,
     pass: process.env.GMAIL_APP_PASSWORD,
