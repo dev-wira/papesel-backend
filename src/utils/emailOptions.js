bcc6da9e105e0bc8c1087276ea9cf20751959334
Rahul <geosagsanbg@gmail.com>
2026-07-11 12:55:49 +0530

    another brevo patch

--- src/utils/emailOptions.js
+++ src/utils/emailOptions.js
@@ -2,14 +2,30 @@ const transporter = require("../configs/transporter");
 const { validate } = require("../models/user");
 const { validateField } = require("../validators/fieldValidation");
 const { appError } = require("./responses");
+const axios = require("axios");
 
-exports.mail = async (to, subject, html) => {
-  await transporter.sendMail({
-    from: process.env.PAPESEL_GMAIL,
-    to,
-    subject,
-    html,
-  });
+exports.mail = async (to, subject, htmlContent) => {
+  console.log(
+    "KEY LOADED:",
+    process.env.BREVO_API_KEY
+      ? "yes, length=" + process.env.BREVO_API_KEY.length
+      : "NO, undefined",
+  );
+  await axios.post(
+    "https://api.brevo.com/v3/smtp/email",
+    {
+      sender: { email: "papeselorg@gmail.com", name: "Papesel" },
+      to: [{ email: to }],
+      subject,
+      htmlContent,
+    },
+    {
+      headers: {
+        "api-key": process.env.BREVO_API_KEY,
+        "Content-Type": "application/json",
+      },
+    },
+  );
 };
 
 exports.verifyEmail = (email) => {
